5c3c8f165e8529c7bdc36796cb45298a1a2b856c
Geoffrey <Geoffrey-Champion@outlook.com>
2022-11-12 20:33:12 +0100

    Ceci est une Calculatrice

---
+++ index.js
@@ -0,0 +1,17 @@
+const buttons = document.querySelectorAll(".btn");
+const result = document.getElementById("result");
+
+buttons.forEach((button) => {
+  button.addEventListener("click", (e) => {
+    result.textContent += e.target.id;
+  });
+});
+
+equal.addEventListener("click", (e) => {
+  result.textContent = eval(result.textContent);
+});
+
+clear.addEventListener("click", (e) => {
+  result.textContent = "";
+});
+console.log(equal);
